f6f6925aca72e97e9221a88eeecd70cda456e5f4
jdbench99 <justin.d.bench@gmail.com>
2021-10-28 18:42:29 -0500

    working

--- lesson7/js/lazyload.js
+++ lesson7/js/lazyload.js
@@ -1,27 +1,27 @@
-const images = document.querySelectorAll("img[data-src]");
+let imagesToLoad = document.querySelectorAll('img[data-src]');
 const loadImages = (image) => {
-    image.setAttribute('src', image.getAttribute('data-src'));
-    image.onload = () => {
-        image.removeAttribute('data.src');
-    }
+  image.setAttribute('src', image.getAttribute('data-src'));
+  image.onload = () => {
+    image.removeAttribute('data-src');
+  };
 };
 imagesToLoad.forEach((img) => {
-    loadImages(img);
+  loadImages(img);
 });
 if('IntersectionObserver' in window) {
-    const observer = new IntersectionObserver((items, observer) => {
-      items.forEach((item) => {
-        if(item.isIntersecting) {
-          loadImages(item.target);
-          observer.unobserve(item.target);
-        }
-      });
+  const observer = new IntersectionObserver((items, observer) => {
+    items.forEach((item) => {
+      if(item.isIntersecting) {
+        loadImages(item.target);
+        observer.unobserve(item.target);
+      }
     });
-    imagesToLoad.forEach((img) => {
-      observer.observe(img);
-    });
-  } else {
-    imagesToLoad.forEach((img) => {
-      loadImages(img);
-    });
-  }
+  });
+  imagesToLoad.forEach((img) => {
+    observer.observe(img);
+  });
+} else {
+  imagesToLoad.forEach((img) => {
+    loadImages(img);
+  });
+}
