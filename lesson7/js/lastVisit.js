acac9571f2067d971c67c2fdcb10cc5ae71ab270
jdbench99 <justin.d.bench@gmail.com>
2021-10-29 17:32:53 -0500

    fix javascript

--- lesson7/js/lastVisit.js
+++ lesson7/js/lastVisit.js
@@ -1,18 +1,20 @@
 //Declare variables
 let div = document.getElementById('lastVisit');
+let daysSince = 0;
+let lastVisit;
 let time = new Date();
 
 
 
 //Create function to populate local storage
-function localStorage() {
+function store() {
     localStorage.setItem('lastVisit', time.getTime());
     localStorage.setItem('currentVisit', time.getTime());
 }
 
 /*Create function to calculate the amount of days that have passed since
 the last visit*/
-function getDays(){
+function getDays() {
     let past = localStorage.getItem('lastVisit');
     let present = localStorage.getItem('currentVisit');
 
@@ -25,7 +27,7 @@ function getDays(){
 }
 
 //Create function to setDate
-function setDate() {
+function setDate(){
     localStorage.setItem('currentVisit', time.getTime());
     daysSince = getDays();
 }
@@ -33,13 +35,13 @@ function setDate() {
 /*if statement to populate storage and calculate days since last visit
 if there is no record of a prior visit*/
 if(!localStorage.getItem('lastVisit')) {
-    localStorage();
+    store();
     daysSince = getDays();
 } else {//if there is a previous visit, create a currentVisit
     setDate();
 }
 
-//display days since last visit to user
+//display the days since the user has been to the page
 div.innerHTML = "You visited this page " + daysSince + " days ago.";
 
 //Set currentVisit as lastVisit
