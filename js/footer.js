3ab96c89800550766f031fdb147dc29ed4b4854c
jdbench99 <justin.d.bench@gmail.com>
2021-11-04 21:16:49 -0500

    lesson 8

--- js/footer.js
+++ js/footer.js
@@ -7,3 +7,32 @@ let yyyy = d.getFullYear();
 let yearSpan = document.querySelector('#year');
 //place current year in span
 yearSpan.textContent = yyyy;
+//create week day array
+let weekDay = new Array(7);
+weekDay[0] = "Sunday";
+weekDay[1] = "Monday";
+weekDay[2] = "Tuesday";
+weekDay[3] = "Wednesday";
+weekDay[4] = "Thursday";
+weekDay[5] = "Friday";
+weekDay[6] = "Saturday";
+//create month array
+let month = new Array(7);
+month[0] = "January";
+month[1] = "February";
+month[2] = "March";
+month[3] = "April";
+month[4] = "May";
+month[5] = "June";
+month[6] = "July";
+month[7] = "August";
+month[8] = "September";
+month[9] = "October";
+month[10] = "November";
+month[11] = "December";
+//turn getday and currentMonth number into string
+let dow = weekDay[d.getDay()];
+let currentMonth = month[d.getMonth()];
+let dateString = dow+", "+dd+" "+currentMonth+" "+yyyy;
+//turn into string
+document.getElementById('currentDate').innerHTML = dateString;
