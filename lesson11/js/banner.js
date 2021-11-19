d4974bffc1941a4d61133dfc92aef448beb923f8
jdbench <justin.d.bench@gmail.com>
2021-11-19 14:01:11 -0600

    fix

--- lesson11/js/banner.js
+++ lesson11/js/banner.js
@@ -1,6 +1,10 @@
 //Display Banner and close banners
 let banner = document.getElementById("banner");
-if (dow=="Friday" && ('https://jdbench.github.io/wdd230/lesson11/preston.html' || 'https://jdbench.github.io/wdd/lesson11/sodasprings.html' || 'https://jdbench.github.io/wdd230/lesson11/fishhaven.html') == window.location.href){
+if (dow=="Friday" && 'https://jdbench.github.io/wdd230/lesson11/preston.html' == window.location.href){
+  banner.style.display = "flex";
+}else if (dow=="Friday" && 'https://jdbench.github.io/wdd230/lesson11/sodasprings.html' == window.location.href){
+  banner.style.display = "flex";
+}else if (dow=="Friday" && 'https://jdbench.github.io/wdd230/lesson11/preston.html' == window.location.href){
   banner.style.display = "flex";
 }
 function exitBanner(){
