a57cbae2c3c653bb7437bad3fd3f3a4aabfa70c6
jdbench <justin.d.bench@gmail.com>
2021-11-30 10:52:42 -0600

    event listener on burger to dropdown on enter

--- lesson12/js/dropdown.js
+++ lesson12/js/dropdown.js
@@ -1,8 +1,19 @@
+const dropdown = document.querySelector('.main-nav');
+document.querySelector('.burger').addEventListener('keypress', function (e) {
+    if (e.key === 'Enter') {
+        if (dropdown.style.display === "flex") {
+            dropdown.style.display = "none";
+        } else {
+            dropdown.style.display = "flex";
+        }
+    }
+
+    }
+);
 function dropdownFunction() {
-    let x = document.querySelector('.main-nav');
-    if (x.style.display === "flex") {
-        x.style.display = "none"
+    if (dropdown.style.display === "flex") {
+        dropdown.style.display = "none"
     } else {
-        x.style.display = "flex"
+        dropdown.style.display = "flex"
     }
 }
