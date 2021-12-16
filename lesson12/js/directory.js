d2e36185c06231168a40a400caa096846e9da0de
jdbench <justin.d.bench@gmail.com>
2021-12-16 04:42:43 -0700

    fixed a couple things

--- lesson12/js/directory.js
+++ lesson12/js/directory.js
@@ -13,7 +13,7 @@ fetch(blackfootApi)
                         <img class="business-logo" src="./images/${business.logo}" alt="business logo for ${business}">
                         <p class="business-desc">${business.description}</p>
                         <a href="tel:${business.phone} class="business-phone"><i class="fa fa-phone"></i>${business.phone}</a>
-                        <p class="business-address">${business.address}</p>
+                        <p class="business-address"><i class="fa fa-map-pin"></i>${business.address}</p>
                         <a href="${business.website} class="business-site"><i class="fa fa-globe"></i>${business.website}</a>
                         <a href="${business.facebook} class="business-facebook"><i class="fa fa-facebook"></i> ${business.facebook_username}</a>
                      </div>`
