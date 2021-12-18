b84aa3b6c1bcdeb0d67850b1c0bbd2bc783c34fa
jdbench <justin.d.bench@gmail.com>
2021-12-18 11:44:55 -0700

    fixed last edit

--- lesson12/js/last-edit.js
+++ lesson12/js/last-edit.js
@@ -1,1 +1,22 @@
-let date = new Date().getFullYear();document.getElementById("last-edit").innerHTML = `Last Updated: ${dateString}`;
+//create week day array
+let weekDay = {
+    0:"Sunday",
+    1:"Monday",
+    2:"Tuesday",
+    3:"Wednesday",
+    4:"Thursday",
+    5:"Friday",
+    6:"Saturday"
+    }
+
+let lastEdit = new Date(document.lastModified);
+
+dd = String(lastEdit.getDate()).padStart(2, '0');
+m = String(lastEdit.getMonth() + 1);
+yyyy = lastEdit.getFullYear();
+
+dow = weekDay[lastEdit.getDay()];
+currentMonth = month[lastEdit.getMonth()];
+dateString = `${dow}, ${dd} ${currentMonth} ${yyyy}`;
+document.getElementById("last-edit").innerHTML = `Last Updated: ${dateString}`;
+
